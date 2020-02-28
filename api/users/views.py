b065e32a8c4123a84a1e61894116f0c1c24c3766
jngcii <concotree@gmail.com>
2020-02-28 19:43:27 +0900

    Debug users registration endpoint

--- api/users/views.py
+++ api/users/views.py
@@ -15,3 +15,5 @@ class UserCreate(APIView):
             user = serializer.save()
             if user:
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
