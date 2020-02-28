2f299e18b173c6c8ccf527e784ad3f09db821824
jngcii <concotree@gmail.com>
2020-02-28 22:05:42 +0900

    Add token auth to view and test

--- api/users/tests.py
+++ api/users/tests.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 class AccountsTest(APITestCase):
     def setUp(self):
@@ -22,6 +23,9 @@ class AccountsTest(APITestCase):
         }
 
         response = self.client.post(self.create_url , data, format='json')
+        user = User.objects.latest('id')
+        token = Token.objects.get(user=user)
+        self.assertEqual(response.data['token'], token.key)
 
         # We want to make sure we have two users in the database..
         self.assertEqual(User.objects.count(), 2)
