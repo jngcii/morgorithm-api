b002d80d81b74c09fbceb0e6c295507a45920821
jngcii <concotree@gmail.com>
2020-03-02 01:23:44 +0900

    Add create group end point to users app

--- users/views.py
+++ users/views.py
@@ -1,18 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserSerializer
-from .models import User
+from .serializers import UserSerializer, GroupSerializer
+# from .models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 
-class UserCreate(APIView):
-    permission_classes = [AllowAny]
+class SignUp(APIView):
     """ 
     Creates the user. 
     """
+    permission_classes = [AllowAny]
 
-    def post(self, request, format='json'):
+    def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
@@ -22,4 +22,20 @@ class UserCreate(APIView):
                 json['token'] = token.key
                 return Response(json, status=status.HTTP_201_CREATED)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateGroup(APIView):
+    """ 
+    Creates the group. 
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = GroupSerializer(data=request.data)
+        if serializer.is_valid():
+            group = serializer.save()
+            if group:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
