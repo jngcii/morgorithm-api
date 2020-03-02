ca149a6050751d05b4bd3e77fd5e0b584d15d8ee
jngcii <concotree@gmail.com>
2020-03-02 17:40:37 +0900

    Add sign in end point in users app

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url('sign-up/', views.SignUp.as_view(), name='sign-up'),
+    url('sign-in/', views.SignIn.as_view(), name='sign-in'),
     url('create-group/', views.CreateGroup.as_view(), name='create-group'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserSerializer, GroupSerializer
-# from .models import User
+from .serializers import UserSerializer, LogInSerializer, GroupSerializer
+from .models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
+
 
 class SignUp(APIView):
     """ 
@@ -23,6 +24,37 @@ class SignUp(APIView):
                 return Response(json, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignIn(APIView):
+    """
+    Log user in
+    """
+    permission_classes = [AllowAny]
+
+    def get_user(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        try:
+            user = User.objects.get(username=username)
+            if user and user.check_password(password):
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
+
+    def post(self, request):
+        serializer = LogInSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = self.get_user(request)
+            if user:
+                token, _ = Token.objects.get_or_create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_200_OK)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class CreateGroup(APIView):
