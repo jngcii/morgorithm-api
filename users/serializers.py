c4d912c73644fbba9af5150c4c17298fc5b800b2
jngcii <concotree@gmail.com>
2020-03-11 22:33:29 +0900

    Fix create method CreateUserSeralizer(UserSeializer)

--- users/serializers.py
+++ users/serializers.py
@@ -16,11 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=8, write_only=True)
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            validated_data['username'],
-            validated_data['email'],
-            validated_data['password']
-        )
+        user = User.objects.create_user(**validated_data)
         return user
 
     class Meta:
@@ -66,4 +62,5 @@ class InitialProfileSerializer(serializers.ModelSerializer):
             'group',
             'problem_groups',
             'problems',
+            'is_confirmed',
         )
