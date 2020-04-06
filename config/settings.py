881dcc5d2f7f6dfd31eeca66940bbad64604ac09
jngcii <concotree@gmail.com>
2020-04-06 17:24:50 +0900

    Update prod setting

--- config/settings.py
+++ config/settings.py
@@ -102,20 +102,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres:///hmc')
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'helpmycode',
-#         'USER': 'root',
-#         'PASSWORD': os.environ.get('POSTGRES_PW'),
-#         'HOST': os.environ.get('POSTGRES_HOST'),
-#         'PORT': '5432'
-#     }
+#     'default': env.db('DATABASE_URL', default='postgres:///hmc')
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'morgorithm',
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PW'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': '5432'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -139,9 +139,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
