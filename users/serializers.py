128bdacc449d7a2d5c2ce2dddf915a07b801622b
jngcii <concotree@gmail.com>
2020-04-25 00:55:54 +0900

    Remove group, avatar field from UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -21,7 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'name', 'password', 'group', 'avatar',)
+        fields = ('id', 'username', 'email', 'name', 'password', )
 
 
 class LogInSerializer(serializers.ModelSerializer):
