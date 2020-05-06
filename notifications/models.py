07db16657148a70fdbea376f792698387e8d7501
jngcii <concotree@gmail.com>
2020-05-06 18:19:09 +0900

    Update notifications model

--- notifications/models.py
+++ notifications/models.py
@@ -5,20 +5,18 @@ from solutions.models import Solution, Comment, TimeStampedModel
 class Notification(TimeStampedModel):
 
     TYPE_CHOICES = (
-        ('like', 'Like'),
-        ('comment', 'Comment'),
+        ('like', 'like'),
+        ('comment', 'comment'),
     )
 
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
-    to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    by = models.ForeignKey(User, on_delete=models.CASCADE)
     notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
     solution = models.ForeignKey(Solution, on_delete=models.CASCADE, null=True, blank=True)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
-    message = models.TextField(null=True, blank=True)
 
     class Meta:
         ordering = ['-created_at']
 
 
     def __str__(self):
-        return 'From:{} - To:{}'.format(self.creator, self.to)
+        return 'From:{} - To:{}'.format(self.by, self.solution.creator)
