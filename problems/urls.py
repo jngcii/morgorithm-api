36b7158d92cabcd978a310bf6de9378a400d310f
jngcii <concotree@gmail.com>
2020-06-02 14:42:07 +0900

    Update path names

--- problems/urls.py
+++ problems/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('init/', views.Init.as_view(), name='init'),
 
     # problem group
-    path('group/', views.ProblemGroupAPI.as_view(), name='problem-group-api'),
-    path('group/<int:group_id>/', views.SingleProblemGroupAPI.as_view(), name='problem-group-api'),
+    path('group/', views.ProblemGroupAPI.as_view(), name='problem_group_api'),
+    path('group/<int:group_id>/', views.SingleProblemGroupAPI.as_view(), name='single_problem_group_api'),
+    
     # path('group/<int:group_id>/<int:prob_id>/', views.GetIsIncluding.as_view(), name='is_including_problem'),
 ]
