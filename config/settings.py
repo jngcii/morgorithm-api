23f6dd32805fd2c1cba0f0ee27477927468bd349
jngcii <concotree@gmail.com>
2020-06-04 23:46:36 +0900

    Change db from RDS to local sqlite3

--- config/settings.py
+++ config/settings.py
@@ -102,20 +102,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 # DATABASES = {
 #     'default': env.db('DATABASE_URL', default='postgres:///hmc')
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'morgorithm',
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PW'),
-        'HOST': env('POSTGRES_HOST'),
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'morgorithm',
+#         'USER': env('POSTGRES_USER'),
+#         'PASSWORD': env('POSTGRES_PW'),
+#         'HOST': env('POSTGRES_HOST'),
+#         'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
